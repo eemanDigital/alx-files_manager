523ff966b0654d031cb448f7734a2a3d94a48abc
eemanDigital <eemandigitalconcept@gamil.com>
2023-12-03 21:05:40 +0100

    task 2 completed

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,20 +2,15 @@ const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 
 exports.getStatus = (req, res) => {
-  if (redisClient.isAlive && dbClient.isAlive) {
-    return res.status(200).json({
-      redis: true,
-      db: true,
-    });
-  }
-  return 'not connected';
+  res.status(200).json({
+    redis: redisClient.isAlive(),
+    db: dbClient.isAlive(),
+  });
 };
 
 exports.getStats = (req, res) => {
-  const users = dbClient.nbUsers;
-  const files = dbClient.nbFiles;
-  return res.status(200).json({
-    users: users.length,
-    files: files.length,
+  res.status(200).json({
+    users: dbClient.nbUsers(),
+    files: dbClient.nbFiles(),
   });
 };
